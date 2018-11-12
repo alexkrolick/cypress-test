5e5e4a3e8974fff8fba4799a5c21d94db537cf76
Alex Krolick <alexander.krolick@gmail.com>
2018-11-11 20:13:24 -0800

    demonstrate chai + jest combined expect

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -3,25 +3,27 @@ import { findByText, findByLabelText } from "dom-testing-addon-async";
 import user from "user-event";
 import * as l from "../../src/localized";
 
-it("successfully loads", async function(done) {
-  return cy.visit("/").then(async function({ document }) {
-    let app = document.querySelector("#app");
-    let rightHeader = await findByText(app, "Hello, world");
-    let wrongHeaders = queryAllByText(app, "Hello, moon");
-    expect(wrongHeaders).toHaveLength(0);
-    expect(rightHeader).not.toBeNull();
-    done();
+describe("Home page", () => {
+  it("successfully loads", async function(done) {
+    cy.visit("/").then(async function({ document: { body } }) {
+      let rightHeader = await findByText(body, "Hello, world");
+      let wrongHeaders = queryAllByText(body, "Hello, moon");
+      expect(wrongHeaders).toHaveLength(0); // jest
+      expect(wrongHeaders).to.have.length(0); // chai
+      expect(rightHeader).not.toBeNull(); // jest
+      expect(rightHeader).to.not.be.null; // chai
+      done();
+    });
   });
-});
 
-it("increments counter", async function(done) {
-  return cy.visit("/").then(async function({ document }) {
-    let app = document.querySelector("#app");
-    let button = await findByText(app, `${l.clickButtonText}`);
-    await user.click(button);
-    expect(
-      await findByLabelText(app, `${l.clickCountLabel}`),
-    ).toHaveTextContent("1");
-    done();
+  it("increments counter", async function(done) {
+    cy.visit("/").then(async function({ document: { body } }) {
+      let button = await findByText(body, `${l.clickButtonText}`);
+      await user.click(button); // ideally, would trigger event with cypress
+      expect(
+        await findByLabelText(body, `${l.clickCountLabel}`),
+      ).toHaveTextContent("1");
+      done();
+    });
   });
 });
